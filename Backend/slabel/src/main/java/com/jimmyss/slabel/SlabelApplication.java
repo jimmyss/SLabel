388aa16e434d7c769501823f9c9016bde7fc7008
jimmyss <3240429693@qq.com>
2024-02-22 20:27:34 +0800

    initialize backend

---
+++ Backend/slabel/src/main/java/com/jimmyss/slabel/SlabelApplication.java
@@ -0,0 +1,13 @@
+package com.jimmyss.slabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlabelApplication.class, args);
+	}
+
+}
